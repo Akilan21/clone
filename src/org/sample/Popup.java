31b34886aadc35d85d0a400c9ae55646114d3498
Akilan21 <akilan2110@gmail.com>
2021-11-12 20:03:46 +0530

    a

---
+++ src/org/sample/Popup.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Popup {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\eclipse-workspace\\Aleart\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.automationtesting.in/Alerts.html");
+		driver.manage().window().maximize();
+		WebElement findElement = driver.findElement(By.xpath("//button[@class='btn btn-danger']"));
+		findElement.click();
+		Thread.sleep(3000);
+		Alert a = driver.switchTo().alert();
+		a.accept();
+		
+	}
+
+}
